979de80dc04ed2a2adf71365287351106604a72f
KASHI TAMANG <tamang.kashi@gmail.com>
2022-04-20 15:17:33 -0700

    added more css elements and started javascript features

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ If you work on more than one feature at a time, you are guaranteed to multiply y
 ## Making a plan
 
 1. **Make a drawing of your app. Simple "wireframes"**
-//see wirefram_about-me.jpg
+//see wireframe_about-me.jpg
 1. **Once you have a drawing, name the HTML elements you'll need to realize your vision**
 -header 
 -main 
--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // import functions
 
-// grab DOM elements
 
+// grab DOM elements
+const animalButton = document.getElementById('animal-button');
+const animalDiv = document.getElementById('animal-div');
 // set event listeners 
+
+
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
+
+animalButton.addEventListener('click', () => {
+    animalDiv.classList.remove('hidden');
+});
