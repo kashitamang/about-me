6889dd85833392dd7589628ede3f64d8f609c6b8
KASHI TAMANG <tamang.kashi@gmail.com>
2022-04-20 16:25:52 -0700

    added event listeners to app.js

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ const animalDiv = document.getElementById('animal-div');
     // use user input to update state 
     // update DOM to reflect the new state
 
+    //removes button hide
+animalButton.classList.remove('hide-button');
+    //event listener for clicking
 animalButton.addEventListener('click', () => {
-    animalDiv.classList.remove('hidden');
+    //shows div
+    animalDiv.classList.toggle('hide-div');
+    animalButton.classList.toggle('hide-button');
 });
+
